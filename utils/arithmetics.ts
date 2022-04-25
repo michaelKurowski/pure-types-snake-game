c7096baffaf4b81abcdf978ef858ea598de000ec
michaelKurowski <Byamarro@gmail.com>
2022-04-25 12:09:51 +0300

    decrementing doubledigits

--- utils/arithmetics.ts
+++ utils/arithmetics.ts
@@ -3,17 +3,32 @@ import { setArrayElement } from './arrays'
 
 type Digit = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
 export type incrementSingleDigit<digit extends number> = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10][digit]
-export type decrementSingleDigit<digit extends number> = [-1, 0, 1, 2, 3, 4, 5, 6, 7][digit]
+export type decrementSingleDigit<digit extends number> = [-1, 0, 1, 2, 3, 4, 5, 6, 7, 8][digit]
 
 //Optimized for tail recursion
 type sumSingleDigit<a extends number, b extends number> = sumSingleDigitHelper<a, b>
 type sumSingleDigitHelper<a extends number, b extends number> = 
   a extends 0 ? b : sumSingleDigitHelper<decrementSingleDigit<a>, incrementSingleDigit<b>> 
 
+type subtractSingleDigit<a extends number, b extends number> = subtractSingleDigitHelper<a, b>
+type subtractSingleDigitHelper<a extends number, b extends number> = 
+  a extends 0 ? b : subtractSingleDigitHelper<decrementSingleDigit<b>, decrementSingleDigit<a>> 
+
+export type decrement<doubleDigit extends DoubleDigit> = decrementHelper<doubleDigit, 0>
+// cannot carry more than one times
+type decrementHelper<doubleDigit extends DoubleDigit, index extends number> =
+  doubleDigit[incrementSingleDigit<index>] extends undefined 
+    ? decrementSingleDigit<doubleDigit[index]> extends -1
+      ? setArrayElement<setArrayElement<doubleDigit, index, 9>, decrementSingleDigit<index>, decrementSingleDigit<doubleDigit[decrementSingleDigit<index>]>>
+      : setArrayElement<doubleDigit, index, decrementSingleDigit<doubleDigit[index]>>
+    : decrementHelper<doubleDigit, incrementSingleDigit<index>>
+
 export type DoubleDigit = Digit[number][]
 
 export type increment<doubleDigit extends DoubleDigit> = incrementHelper<doubleDigit, 0>
 
+
+// cannot carry more than one times
 type incrementHelper<doubleDigit extends DoubleDigit, index extends number> =
   doubleDigit[incrementSingleDigit<index>] extends undefined 
     ? incrementSingleDigit<doubleDigit[index]> extends 10
--- utils/arrays.ts
+++ utils/arrays.ts
@@ -4,6 +4,7 @@ export type setArrayElement<arr extends Array<any>, index extends number, value>
   [key in keyof arr]: key extends `${index}` ? value : arr[key]
 }
 
+
 type GetArrayLengthWrapper<arr extends DoubleDigit> = GetArrayLength<arr, 0>
 type GetArrayLength<arr extends DoubleDigit, index extends number> =
   arr[index] extends undefined ? index : GetArrayLength<arr,  incrementSingleDigit<index>>
