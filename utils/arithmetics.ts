d207e12547c193d5f2b61d31da970c15a9f2cdd6
Marcin Grzeszczak <marcin.grzeszczak+github@gmail.com>
2022-04-26 17:14:50 +0200

    Multidigit increment and decrement. Carry issue fix.

--- utils/arithmetics.ts
+++ utils/arithmetics.ts
@@ -14,25 +14,34 @@ type subtractSingleDigit<a extends number, b extends number> = subtractSingleDig
 type subtractSingleDigitHelper<a extends number, b extends number> = 
   a extends 0 ? b : subtractSingleDigitHelper<decrementSingleDigit<b>, decrementSingleDigit<a>> 
 
-export type decrement<doubleDigit extends DoubleDigit> = decrementHelper<doubleDigit, 0>
+export type decrement<doubleDigit extends DoubleDigit> = decrementHelper<doubleDigit, decrementSingleDigit<doubleDigit["length"]>>
+
+type decrementHelper<doubleDigit extends DoubleDigit, index extends number> = index extends -1 ? doubleDigit : 
+doubleDigit[index] extends 0 ? 
+decrementHelper<setArrayElement<doubleDigit, index, 9>, decrementSingleDigit<index>> :
+setArrayElement<doubleDigit, index, decrementSingleDigit<doubleDigit[index]> >
+
 // cannot carry more than one times
-type decrementHelper<doubleDigit extends DoubleDigit, index extends number> =
-  doubleDigit[incrementSingleDigit<index>] extends undefined 
-    ? decrementSingleDigit<doubleDigit[index]> extends -1
-      ? setArrayElement<setArrayElement<doubleDigit, index, 9>, decrementSingleDigit<index>, decrementSingleDigit<doubleDigit[decrementSingleDigit<index>]>>
-      : setArrayElement<doubleDigit, index, decrementSingleDigit<doubleDigit[index]>>
-    : decrementHelper<doubleDigit, incrementSingleDigit<index>>
+// type decrementHelper<doubleDigit extends DoubleDigit, index extends number> =
+//   doubleDigit[incrementSingleDigit<index>] extends undefined 
+//     ? decrementSingleDigit<doubleDigit[index]> extends -1
+//       ? setArrayElement<setArrayElement<doubleDigit, index, 9>, decrementSingleDigit<index>, decrementSingleDigit<doubleDigit[decrementSingleDigit<index>]>>
+//       : setArrayElement<doubleDigit, index, decrementSingleDigit<doubleDigit[index]>>
+//     : decrementHelper<doubleDigit, incrementSingleDigit<index>>
 
 export type DoubleDigit = Digit[number][]
 
-export type increment<doubleDigit extends DoubleDigit> = incrementHelper<doubleDigit, 0>
+export type increment<doubleDigit extends DoubleDigit> = incrementHelper<doubleDigit, decrementSingleDigit<doubleDigit["length"]>>
 
+type incrementHelper<doubleDigit extends DoubleDigit, index extends number> = index extends -1 ? doubleDigit : 
+doubleDigit[index] extends 9 ?  
+incrementHelper<setArrayElement<doubleDigit, index, 0>, decrementSingleDigit<index>> : 
+setArrayElement<doubleDigit, index, incrementSingleDigit<doubleDigit[index]>>
 
-// cannot carry more than one times
-type incrementHelper<doubleDigit extends DoubleDigit, index extends number> =
-  doubleDigit[incrementSingleDigit<index>] extends undefined 
-    ? incrementSingleDigit<doubleDigit[index]> extends 10
-      ? setArrayElement<setArrayElement<doubleDigit, index, 0>, decrementSingleDigit<index>, incrementSingleDigit<doubleDigit[decrementSingleDigit<index>]>>
-      : setArrayElement<doubleDigit, index, incrementSingleDigit<doubleDigit[index]>>
-    : incrementHelper<doubleDigit, incrementSingleDigit<index>>
-
+// // cannot carry more than one times
+// type incrementHelper<doubleDigit extends DoubleDigit, index extends number> =
+//   doubleDigit[incrementSingleDigit<index>] extends undefined 
+//     ? incrementSingleDigit<doubleDigit[index]> extends 10
+//       ? setArrayElement<setArrayElement<doubleDigit, index, 0>, decrementSingleDigit<index>, incrementSingleDigit<doubleDigit[decrementSingleDigit<index>]>>
+//       : setArrayElement<doubleDigit, index, incrementSingleDigit<doubleDigit[index]>>
+//     : incrementHelper<doubleDigit, incrementSingleDigit<index>>
